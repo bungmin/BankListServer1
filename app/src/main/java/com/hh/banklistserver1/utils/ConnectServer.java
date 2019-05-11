f078293ecdbac8d7ae8a0c4b32e2c62062669be8
babobungmin <babobungmin@naver.com>
2019-05-11 15:44:32 +0900

    은행 정보 api 호출 예시   - get 방식, 필요 파라미터가 없는 경우

--- app/src/main/java/com/hh/banklistserver1/utils/ConnectServer.java
+++ app/src/main/java/com/hh/banklistserver1/utils/ConnectServer.java
@@ -1,8 +1,18 @@
 package com.hh.banklistserver1.utils;
 
 import android.content.Context;
+import android.util.Log;
 
 import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ConnectServer {
 
@@ -18,6 +28,34 @@ public class ConnectServer {
 
     // 필요한 서버 요청들을 하나한 메ㅗㅆ드로 만들기
     public static void getRequestInfoBank(Context context, /* 필요한파라미터용 변수들 */ final JsonResponseHandler handler){
+
+        // 서버 - 클라이언트 (앱)
+
+        OkHttpClient client = new OkHttpClient();
+
+        //URL 설정 => 목적지 설정
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(BASE_URL + "/info/bank").newBuilder();
+
+        String requestURL = urlBuilder.build().toString();
+
+        //완성된 url로 접근하는 request를 생성.
+        Request request = new Request.Builder().url(requestURL).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String responseContent = response.body().string();
+
+                Log.d("서버 응답 내용", responseContent);
+
+            }
+        });
 
 
 
